2b725f32e3bd26f25b3af45085edd05cedb35cbb
Ethan Kisiel <ethan.a.kisiel@gmail.com>
2022-10-26 12:40:37 -0400

    Initial Commit

---
+++ StarscreamTest/StarscreamTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarscreamTestApp.swift
+//  StarscreamTest
+//
+//  Created by Ethan Kisiel on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct StarscreamTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
